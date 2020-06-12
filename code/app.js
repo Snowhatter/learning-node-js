fb555da56d038a84ea3d795faa9f1428fddce8d3
Krishnamurthy <karjun@kpmg.com>
2020-06-12 12:59:17 -0400

    lesson-1

--- code/app.js
+++ code/app.js
@@ -1,3 +1,15 @@
-const name = 'Arjun'
+// const addthings = require('./utils.js')
+// const firstname = require('./utils.js')
+const takeNotes = require('./notes.js')
 
-console.log(name)
+// const name = 'Arjun'
+// const sum = addthings(2,4)
+const note = takeNotes()
+
+// console.log(sum)
+ console.log(note)
+// console.log(firstname)
+// console.log(name)
+//const getNotes = require('./notes.js')
+//const msg = getNotes()
+//console.log(msg)
---
+++ code/utils.js
@@ -0,0 +1,8 @@
+console.log('utils.js - message')
+const name = 'Jack'
+const add = function(a,b){
+    return a + b
+}
+
+module.exports = name
+module.exports = add
