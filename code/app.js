58b565971381c43b79f4871724b39bdaff20c29f
Krishnamurthy <karjun@kpmg.com>
2020-06-15 11:17:55 -0400

    Chalk module
    Signed-off-by: Krishnamurthy <karjun@kpmg.com>

--- code/app.js
+++ code/app.js
@@ -2,6 +2,7 @@ const validator = require('validator')
 // const addthings = require('./utils.js')
 // const firstname = require('./utils.js')
 const takeNotes = require('./notes.js')
+const chalk = require('chalk')
 
 // const name = 'Arjun'
 // const sum = addthings(2,4)
@@ -9,3 +10,7 @@ const note = takeNotes()
 
 console.log(note)
 console.log(validator.isURL('monkey'))
+
+msg = chalk.bold.green('Success!')
+
+console.log(msg)
